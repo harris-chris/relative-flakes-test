d9d3263de7c29dbdd69f9866be0db29dee264d6f
harris-chris <chrisharriscjh@gmail.com>
2022-05-19 13:04:25 +0900

    Add cbonsai slow build

--- flake.nix
+++ flake.nix
@@ -2,14 +2,14 @@
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-21.11;
     flake-utils.url = github:numtide/flake-utils;
-    cbonsai.url = git+file:./cbonsai;
+    cbonsai.url = path:./cbonsai;
   };
   outputs = { self, nixpkgs, flake-utils, cbonsai }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in rec {
-        packages = cbonsai.defaultPackage.${system};
+        packages.cbonsai = cbonsai.defaultPackage.${system};
       });
 }
 
